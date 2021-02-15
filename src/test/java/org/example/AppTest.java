8e50b0bc8354d1f49b629a49a2dc7ec222261ad4
tianwen123 <1163316096@qq.com>
2021-02-16 02:51:10 +0800

    2

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,17 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
+import org.testng.annotations.Test;
 
-import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue(){
+        System.out.println("你好");
     }
 }
