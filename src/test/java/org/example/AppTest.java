be56b5e8bb40d6d0223d3b61f5b5f6bf728f5ef8
tianwen123 <1163316096@qq.com>
2021-02-16 21:25:09 +0800

    4

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -10,8 +10,12 @@ public class AppTest {
     /**
      * Rigorous Test :-)
      */
-    @Test(description = "diyige",testName = "第一个name")
+    @Test(description = "第一个描述",testName ="第一个名字")
     public void shouldAnswerWithTrue(){
+        System.out.println("你好");
+    }
+    @Test(description = "第2个描述",testName ="第2个名字")
+    public void shouldAnswerWithTrue2(){
         System.out.println("你好");
     }
 }
